74936841239feaa0b1265e1e3bd506318696754f
Nierowheezy <olaniyiolabode99@yahoo.com>
2020-06-09 02:43:01 +0100

    first commit

--- src/Timer.js
+++ src/Timer.js
@@ -1,32 +1,27 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from 'react';
 
-class Timer extends Component {
-  state = {
-    clock: 0
+const Timer = ({ start }) => {
+  const initialState = 0;
+  const [clock, setClock] = useState(initialState);
+
+  const Ticker = () => {
+    setClock(new Date() - start);
   };
 
-  Ticker = () => {
-    this.setState({
-      clock: new Date() - this.props.start
-    });
-  };
+  let clockVal = Math.round(clock / 1000);
 
-  componentDidMount() {
-    this.timer = setInterval(this.Ticker, 1000);
-  }
+  useEffect(() => {
+    setInterval(Ticker, 1000);
+  }, []);
 
-  render() {
-    const { clock } = this.state;
-    let clockVal = Math.round(clock / 1000);
-    return (
-      <div>
-        <h2>You have been on this site since :</h2>
-        <br />
-        <h3>{clockVal}</h3>
-        <p>Seconds</p>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h2>You have been on this site since :</h2>
+      <br />
+      <h3>{clockVal}</h3>
+      <p>Seconds</p>
+    </div>
+  );
+};
 
 export default Timer;
